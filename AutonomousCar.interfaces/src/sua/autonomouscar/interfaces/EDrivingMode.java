eeae07c34f5fa71990596e11194f4bb7fbc43428
VicenteMurguiSanchis <vtemurguis@gmail.com>
2021-05-19 17:09:28 +0200

    [UPD]: Enumerates de la propiedades

--- AutonomousCar.interfaces/src/sua/autonomouscar/interfaces/EDrivingMode.java
+++ AutonomousCar.interfaces/src/sua/autonomouscar/interfaces/EDrivingMode.java
@@ -1,5 +1,11 @@
 package sua.autonomouscar.interfaces;
 
 public enum EDrivingMode {
-
+	L0_ManualDriving,
+	L1_AssistedDriving,
+	L2_AdaptiveCruiseControl,
+	L2_LaneKeepingAssist,
+	L3_HighwayChauffer,
+	L3_TrafficJamChauffer,
+	L3_CityChauffer
 }
--- AutonomousCar.interfaces/src/sua/autonomouscar/interfaces/ESensorStatus.java
+++ AutonomousCar.interfaces/src/sua/autonomouscar/interfaces/ESensorStatus.java
@@ -1,5 +1,8 @@
 package sua.autonomouscar.interfaces;
 
 public enum ESensorStatus {
-
+	Lidar_error,
+	OK,
+	Distance_sensor_error,
+	No_Distance_Sensor
 }

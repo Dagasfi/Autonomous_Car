979fb20419babc87310a722ca54a0083ec60d08d
Dagasfi <daniel.gaspar98@gmail.com>
2021-05-19 16:35:39 +0200

    [UPD]: añadimos un metodo a los monitores

--- AutonomousCar.interfaces/src/sua/autonomouscar/monitors/interfaces/IMonitor.java
+++ AutonomousCar.interfaces/src/sua/autonomouscar/monitors/interfaces/IMonitor.java
@@ -5,5 +5,7 @@ import sua.autonomouscar.interfaces.IIdentifiable;
 public interface IMonitor  extends IIdentifiable {
 	
 	public void notifyRule(String ruleName);
+	
+	public void changeProperties();
 
 }

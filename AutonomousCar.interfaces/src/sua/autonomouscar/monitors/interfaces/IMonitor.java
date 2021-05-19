cac918ce7f61eef110d86a4010ffcdfc3f381b61
Dagasfi <daniel.gaspar98@gmail.com>
2021-05-19 13:11:14 +0200

    [ADD]: Interfaz de monitores

---
+++ AutonomousCar.interfaces/src/sua/autonomouscar/monitors/interfaces/IMonitor.java
@@ -0,0 +1,9 @@
+package sua.autonomouscar.monitors.interfaces;
+
+import sua.autonomouscar.interfaces.IIdentifiable;
+
+public interface IMonitor  extends IIdentifiable {
+	
+	public void notifyRule(String ruleName);
+
+}
